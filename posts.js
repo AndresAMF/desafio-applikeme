14c8f70469e288ad1a12a6313900c623044f1c3f
AndresAMF <andres.1991.munoz@gmail.com>
2023-01-14 16:57:58 -0300

    se agrega likes y delete post

--- posts.js
+++ posts.js
@@ -1,26 +1,44 @@
-const {Pool} = require('pg')
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'sudo',
-    database: 'likeme',
-    allowExitOnIdle: true
-})
+  host: "localhost",
+  user: "postgres",
+  password: "sudo",
+  database: "likeme",
+  allowExitOnIdle: true,
+});
 
+const obtenerPosts = async () => {
+  const { rows } = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+  return rows;
+};
 
-const agregarPosts = async(titulo, url, descripcion, likes) => {
-    const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3, $4)"
-    const values = [titulo, url, descripcion, likes]
-    await pool.query(consulta, values)
-    console.log("Post agregado exitosamente")
-}
+const agregarPosts = async (titulo, url, descripcion, likes) => {
+  likes = null ? (likes = 0) : (likes = 0);
+  const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3, $4)";
+  const values = [titulo, url, descripcion, likes];
+  await pool.query(consulta, values);
+  console.log("Post agregado exitosamente");
+};
 
+const editarLike = async (id) => {
+  const consulta = "UPDATE posts SET likes = likes + 1 WHERE id=$1";
+  const values = [id];
+  await pool.query(consulta, values);
+  console.log(`Se ha agregado un like al post con id: ${id} exitosamente`);
+};
 
-const obtenerPosts = async() => {
-    const {rows} = await pool.query("SELECT * FROM posts")
-    console.log(rows)
-    return rows
-}
+const eliminarPost = async (id) => {
+  const consulta = "DELETE FROM posts WHERE id = $1";
+  const values = [id];
+  await pool.query(consulta, values);
+  console.log(`Post con ID ${id} eliminado.`);
+};
 
-module.exports = {agregarPosts, obtenerPosts}
+module.exports = {
+  agregarPosts,
+  obtenerPosts,
+  editarLike,
+  eliminarPost,
+};
--- server.js
+++ server.js
@@ -1,27 +1,63 @@
-const {agregarPosts, obtenerPosts} = require('./posts')
-const express = require('express')
+const {
+  agregarPosts,
+  obtenerPosts,
+  editarLike,
+  eliminarPost,
+} = require("./posts");
+
+const express = require("express");
 const cors = require("cors");
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
+app.listen(3000, console.log("Servidor encendido"));
 
-app.listen(3000, console.log("Servidor encendido"))
+app.get("/", () => {
+  try {
+    res.sendFile(`${__dirname}/index.html`);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-app.get('/', () => {
-    res.sendFile(`${__dirname}/index.html`)
-})
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await obtenerPosts();
+    res.json(posts);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-app.get('/posts', async(req, res) => {
-    const posts = await obtenerPosts()
-    res.json(posts)
-})
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion, likes } = req.body;
+    await agregarPosts(titulo, url, descripcion, likes);
+    res.send("Post agregado exitosamente.");
+  } catch (error) {
+    res.send(error);
+  }
+});
 
+app.put("/posts/like/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await editarLike(id);
+    res.send("Actualización exitosa.");
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-app.post('/posts', async(req, res) => {
-    const {titulo, url, descripcion, likes} = req.body
-    await agregarPosts(titulo, url, descripcion, likes)
-    res.send("Post agregado exitosamente")
-})
+app.delete("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await eliminarPost(id);
+    res.send(`Post con ID ${id} eliminado.`);
+  } catch (error) {
+    res.send(error);
+  }
+});
